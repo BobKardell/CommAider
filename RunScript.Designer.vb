27e2e5c5bb71c806f7c81dc96820b4ccd703da1b
Bob Kardell <70972023+BobKardell@users.noreply.github.com>
2020-09-08 12:51:28 -0500

    Add files via upload

---
+++ RunScript.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RunScript
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RunScript))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 42)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(728, 31)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(12, 114)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(728, 197)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Initial Script"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(9, 349)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(731, 39)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 329)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Editted Script"
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(366, 402)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 50)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Run!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(464, 399)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(114, 24)
+        Me.RadioButton1.TabIndex = 7
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "and prompt"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(464, 426)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(102, 24)
+        Me.RadioButton2.TabIndex = 8
+        Me.RadioButton2.Text = "and close"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Script Options"
+        '
+        'RunScript
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(770, 464)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "RunScript"
+        Me.Text = "Script Edit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label3 As Label
+End Class
---
+++ RunScript.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class RunScript
+    Private Sub RunScript_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = Form1.ComboBox1.Text
+
+        Dim list As New List(Of String)
+        Dim matches As MatchCollection = Regex.Matches(TextBox1.Text, "\[\w+\]")
+
+        For Each m As Match In matches
+            List.Add(m.Value)
+        Next
+
+        list = list.Distinct.ToList
+
+        Dim newbox As TextBox
+        Dim newlabel As Label
+
+        For i As Integer = 1 To list.Count
+
+            'create a new textbox and set its properties
+            newlabel = New Label
+            newlabel.Size = New Drawing.Size(190, 30)
+            newlabel.Location = New System.Drawing.Point(10, 45 + (35 * (i - 1)))
+            newlabel.Name = "Label" & i
+            newlabel.Text = list(i - 1)
+
+            newbox = New TextBox
+            newbox.Size = New Drawing.Size(190, 30)
+            newbox.Location = New System.Drawing.Point(200, 45 + (35 * (i - 1)))
+            newbox.Name = "TextBox" & i
+            'newbox.Text = list(i - 1)
+
+            'connect it to a handler, save a reference to the array and add it to the form controls
+
+            'AddHandler newbox.TextChanged, AddressOf newbox_TextChanged
+            'boxes(i) = newbox
+
+            AddHandler newbox.TextChanged, AddressOf newbox_TextChanged
+
+            Panel1.Controls.Add(newlabel)
+            Panel1.Controls.Add(newbox)
+
+        Next
+
+    End Sub
+    Private Sub newbox_TextChanged(ByVal sender As Object, ByVal e As EventArgs)
+        Dim matches As MatchCollection = Regex.Matches(TextBox1.Text, "\[\w+\]")
+        'MsgBox(matches.Count)
+        'Dim list As New List(Of String)
+
+        'For Each m As Match In matches
+        'list.Add(m.Value)
+        'Next
+
+        'list = list.Distinct.ToList
+
+
+        'For i As Integer = 1 To list.Count
+        Dim tb_value As String
+        Dim lb_value As String
+        Dim i As String
+        i = Strings.Right(Me.ActiveControl.Name, 1)
+
+        Dim textboxvalue As TextBox = CType(Panel1.Controls("TextBox" & i), TextBox)
+            tb_value = textboxvalue.Text
+            Dim labelvalue As Label = CType(Panel1.Controls("Label" & i), Label)
+            lb_value = labelvalue.Text
+
+            TextBox2.Text = Replace(TextBox1.Text, lb_value, tb_value)
+
+            Me.Refresh()
+        'Next
+
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Shell("com.exe" & TextBox2.Text, AppWinStyle.NormalFocus)
+        If RadioButton1.Checked Then
+            Process.Start("cmd", "/k" & TextBox2.Text)
+        End If
+
+        If RadioButton2.Checked Then
+            Process.Start("cmd", "/c" & TextBox2.Text)
+        End If
+
+    End Sub
+
+End Class
